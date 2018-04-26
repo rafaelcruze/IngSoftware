0d9ecc027a91adab3ebcf872a86224b6797e533a
Rafael Cruz <rafael.cruze@utem.cl>
2018-04-25 23:13:39 -0300

    ADD modelo tipo de usuario con su respectiva migracion
    Signed-off-by: Rafael Cruz <rafael.cruze@utem.cl>

---
+++ db/migrate/20180426021106_create_usuarios.rb
@@ -0,0 +1,17 @@
+class CreateUsuarios < ActiveRecord::Migration[5.1]
+  def change
+    create_table :usuarios do |t|
+      t.string :nombres
+      t.string :apellidos
+      t.string :rut
+      t.string :email
+      t.string :contraseña
+      t.date :fecha_nacimiento
+      t.string :direccion
+      t.integer :comuna_id
+      t.integer :tipo_usuario_id
+
+      t.timestamps
+    end
+  end
+end
