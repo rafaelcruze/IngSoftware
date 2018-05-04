c0e53c74fb4e699e6ee6fd9008df7dae9315ac5e
Rafael Cruz <rafael.cruze@utem.cl>
2018-05-04 16:38:54 -0300

    ADD: Subiendo algunas validaciones de estudiante
    Signed-off-by: Rafael Cruz <rafael.cruze@utem.cl>

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -4,4 +4,12 @@ class Estudiante < ApplicationRecord
   belongs_to :comuna #un estudiante puede tener asociado una comuna
   belongs_to :jornada #un estudiante puede ser asociada una jornada
   belongs_to :nivel_educacional #un estudiante puede ser asociado un nivel educacional
+
+  validates :nombres , presence:  { message: 'Ingrese los nombres '} , length: {in: 3..20}
+  validates :apellidos , presence: { message: 'Ingrese los apellidos'}, length: {in: 3..20}
+  validates :rut , presence: { message: 'Ingrese un rut'}, uniqueness: true
+  validates :comuna_id , presence: { message: 'Seleccione alguna comuna'}
+  validates :jornada_id , presence: { message: 'Selecciones alguna jornada'}
+  validates :nivel_educacional_id , presence: { message: 'Seleccione algún nivel educacional'}
+  validates :fecha_nacimiento , presence: {message: 'Ingrese la fecha de nacimiento'}
 end
