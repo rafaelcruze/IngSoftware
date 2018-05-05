58de2649e9975f5a4cdcc13d34c1624ef99ff89b
Melody Alhambra <melody.alhambras@utem.cl>
2018-05-04 23:53:38 -0300

    ADD: Subiendo algunas validaciones de usuario
    Signed-off-by: Melody Alhambra <melody.alhambras@utem.cl>

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,14 @@ class Usuario < ApplicationRecord
   belongs_to :comuna #usuario puede estar asociado a una comuna
   has_many :estudiantes #usuario puede tener asociado a uno o muchos estudiantes
   has_many :pagos #usuario puede estar asociado a muchos pagos
+
+  validates :nombres , presence: { message: 'Ingrese los nombres '} , length: {in: 3..20}
+  validates :apellidos , presence: { message: 'Ingrese los apellidos '} , length: {in: 3..20}
+  validates :direccion , presence: { message: 'Ingrese dirección'} , length: {in: 3..30}
+  validates :email , presence: { message: 'Ingrese email'} , uniqueness: true
+  validates :fecha_nacimiento , presence: { message: 'Ingrese la fecha de nacimiento'}
+  validates :contraseña , presence: { message: 'Ingrese contraseña'} , length: {in: 6..8}
+  validates :rut , presence: { message: 'Ingrese rut'}, uniqueness: true
+  validates :comuna_id , presence: { message: 'Selecciones comuna'}
+  validates :tipo_usuario_id , presence: { message: 'Selecciones tipo de usuario'}
 end
