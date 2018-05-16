20f1b277b634f198bdfc855cb504e40b9200108f
Rafael Cruz <rafael.cruze@utem.cl>
2018-05-15 21:23:36 -0400

    Se suben algunas validaciones de modelo Pago
    Signed-off-by: Rafael Cruz <rafael.cruze@utem.cl>

--- app/models/pago.rb
+++ app/models/pago.rb
@@ -3,4 +3,13 @@ class Pago < ApplicationRecord
   belongs_to :estado #pago puede tener asociado uno estado
   belongs_to :usuario #pago puede estar asociado a un usuario
   belongs_to :estudiante #pago puede estar asociado a un estudiante
+
+  validates :valor , presence: { message: 'Ingrese un valor'}
+  validates :abono , presence: { message: 'Ingrese un abono'}
+  validates :saldo , presence: { message: 'Ingrese un saldo'}
+  validates :fecha_pago , presence: { message: 'Ingrese una fecha de pago'}
+  validates :tipo_pago_id , presence: { message: 'Seleccione tipo de pago'}
+  validates :estado_id , presence: { message: 'Seleccione estado de pago'}
+  validates :usuario_id , presence: { message: 'Seleccione al apoderado'}
+  validates :estudiante_id , presence: { message: 'Seleccione al estudiante'}
 end
