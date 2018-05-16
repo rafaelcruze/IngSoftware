e67bde0238196842d2cbc0777e332d9e049f5657
Rafael Cruz <rafael.cruze@utem.cl>
2018-05-15 21:09:36 -0400

    Se sube validaciones de modelo agenda
    Signed-off-by: Rafael Cruz <rafael.cruze@utem.cl>

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,4 +1,13 @@
 class Agenda < ApplicationRecord
   belongs_to :usuario
   belongs_to :estudiante
+
+  validates :comentario, presence: { message: 'Ingrese algún comentario'}
+  validates :fecha, presence: { message: 'Ingresa alguna fecha'}
+
+  def fecha_de_comentario
+    if !fecha.blank? and fecha < Date.today
+      errors.add(:fecha, "Ingresaste fecha que ya pasó")
+    end
+  end
 end
