3f9288e7485bd80ff01516977b3572a9a9ac6a8a
Melody Alhambra <melody.alhambras@utem.cl>
2019-01-21 15:58:48 -0300

    Se realiza arreglo

--- db/migrate/20190121185134_changecolumnpagos.rb
+++
@@ -1,6 +0,0 @@
-class Changecolumnpagos < ActiveRecord::Migration[5.1]
-  def change
-    change_column :pagos, :valor, :integer, :null => false
-    change_column :pagos, :abono, :integer, :null => false
-  end
-end
